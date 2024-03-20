e495ed2020c1e4905656b9a65d5be10f567655ef
Carlos Faustino <chfcchfc96@gmail.com>
2024-03-20 12:00:24 -0300

    feat: add input component

---
+++ src/components/input.tsx
@@ -0,0 +1,27 @@
+import { ComponentProps } from "react"
+
+import { cn } from "../utils/cn"
+
+interface InputProps extends ComponentProps<"input"> {
+  label: string
+}
+
+export function Input(props: Readonly<InputProps>) {
+  return (
+    <div className="relative flex items-center">
+      <input
+        {...props}
+        className="p-3 outline-0 rounded bg-base-input border border-base-button focus:border-yellow-dark placeholder:text-base-label text-base-text font-roboto text-sm flex-1 peer"
+        placeholder={props.label}
+      />
+      <p
+        className={cn(
+          "absolute right-3 italic text-xs text-base-label font-roboto hidden",
+          !props.required && "block peer-focus:hidden peer-placeholder-shown:block",
+        )}
+      >
+        Opcional
+      </p>
+    </div>
+  )
+}
